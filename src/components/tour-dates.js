3e8bdbe34f99122f4e0ddb7b343956f41a89261c
Jesse Higson <jesse@superrb.com>
2021-12-03 15:22:29 +0000

    Getting tour dates section working

--- src/components/tour-dates.js
+++ src/components/tour-dates.js
@@ -1,22 +1,25 @@
-import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
+import { Helmet } from 'react-helmet'
 
 const TourDates = () => (
   <section className="tour-dates">
     <div className="tour-dates__container">
+      <h4 className="tour-dates__title">Tour Dates</h4>
+
       <a
         href="https://www.songkick.com/artists/27184"
         class="songkick-widget"
         data-theme="dark"
         data-track-button="on"
         data-detect-style="true"
-        data-font-color="#FFFFFF"
+        data-font-color="#ffffff"
         data-background-color="transparent"
         data-locale="en"
-      >
-        Tour dates
-      </a>
-      <script src="//widget.songkick.com/27184/widget.js"></script>
+      ></a>
+
+      <Helmet>
+        <script src="//widget.songkick.com/27184/widget.js"></script>
+      </Helmet>
     </div>
   </section>
 )
