934dc2f523e15273d92157adad31925e4e351468
Jesse Higson <jesse@superrb.com>
2021-12-09 23:48:12 +0000

    feat: updating bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,19 +6,25 @@ const Bio = () => (
     <div className="bio__container">
       <div className="bio__column bio__column--content">
         <p>
-          Sven are an Alternative/Emo band made up of Steve, Jesse, Luke and
-          George, they formed in Portsmouth, UK in 2017.
+          Sven are a four-piece Alternative/Emo band hailing from Portsmouth,
+          UK.
         </p>
         <p>
-          Their first EP ‘Seventeen’ was self released in February of 2018 and
-          since then they’ve played with established acts including Touché
-          Amoré, Nothing and The Dangerous Summer. ‘Seventeen’ has since been
-          reissued on cassette through Blindreader Records (UK) and Mourning
-          Records (USA).
+          They combine a mix of melancholy lyrics with powerful guitar led
+          melodies to deliver an emotive sound reminiscent of 90’s alt-rock,
+          whilst drawing influence from shoegaze and the modern emo revival.
         </p>
         <p>
-          Sven’s newest single ‘Disdain’ was released via Blindreader Records
-          (UK) on November 2nd 2018.
+          Since bursting onto the scene with their self released EP ‘Seventeen’
+          in 2017, they have played across the UK with a variety of bands
+          including established international acts such as Touché Amoré, Nothing
+          and The Dangerous Summer. In late 2018, Sven released a two-track
+          titled ‘Disdain’ via Blindreader Records.
+        </p>
+        <p>
+          Post pandemic, Sven are back and ready to reach new heights,
+          delivering their long awaited third release ‘Burn’, produced by Ben
+          Sansom (Lower Than Atlantis).
         </p>
       </div>
 
